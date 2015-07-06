f99ee4178b4e1f776af9d51cbb52e4567f10b125
Rodolfo Gutierrez <rodolf.gutierrez@gmail.com>
2015-07-06 16:53:06 -0700

    " ============================================================================ " Netrw Directory Listing                                        (netrw v140) "   /Users/Rodolfo/Documents/Cse/GitWebsite/rodolfo12.github.io/_site/about "   Sorted by      name "   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$ "   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec " ============================================================================ ../ index.html .swp

--- p1/p1.cs
+++ p1/p1.cs
@@ -1,20 +1,27 @@
+//Problem: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
 using System; 
 
-public class hello {
+public class p1 {
 	
 	public static void Main(string[] args)
 	{
 	
-	int temp = 0;
+	int sum = 0;
 		
+		//For loop iterates through 0-1000. 
 		for (int i=0; i < 1000; i++)
 		{
+			//If integer "i" has a 0 modulous for 3 OR 5, store integer in "sum" variable
+			//and print the value of "i" at this iteration.
 			if (i % 3 == 0 || i % 5 == 0)
 			{
-				temp += i; 
+				sum += i; 
 				Console.WriteLine(i);
 			}
 		}
-		Console.WriteLine("Sum:{0} ", temp);
+		//print the sum of all integer values with a 0 modulous for 3 OR 5.
+		Console.WriteLine("Sum:{0} ", sum);
 	}
 }
