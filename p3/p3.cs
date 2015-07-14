48f0a0aa00b41ade428b617f1be200853d94cd84
Rodolfo Gutiérrez <rodolf.gutierrez@gmail.com>
2015-07-13 22:04:01 -0700

    Update p3.cs

--- p3/p3.cs
+++ p3/p3.cs
@@ -2,45 +2,37 @@ using System;
 
 public  class LargestPrimeFactor {
 	
-	public static void Main()
-	{	
-				 	long a=0, // value we want to find prime factor of
-				   		b=2, // divisor
-						c=0; // store value
+	public static void Main() {	
+		long a=0, // value we want to find prime factor of
+		     b=2, // divisor
+	     	     c=0; // store value
 
-			
 		string input = Console.ReadLine();
 			//grab input for N. convert input to double. 
 
-		if (long.TryParse(input, out a))
-		{
+		if (long.TryParse(input, out a)) {
 			Console.WriteLine("Input value: {0}", a);	
 		}
-		else
-		{
+		else {
 			Console.WriteLine("Error");
 		}
-			//find square of input.
-		
-	while (b<=(a))
-	{
-		if ( a % b == 0)
-		{
-			a = (a/b);
-			c = b;
+			
+	//find square of input.
+	while (b<=(a))	{
+	
+		if ( a % b == 0) {
+			a=(a/b);
+			c=b;
 			b=2;
 
 			Console.WriteLine("value of a: {0}", a);
 			Console.WriteLine("value of c: {0}", c);
-
 		}
-		else 
-		{ 
+		else { 
 			b++; 
 			Console.WriteLine("value of b: {0}", b);
 		} 
 	}
-
 
 	}
 
